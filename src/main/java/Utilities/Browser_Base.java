d3e022781896fdb86fe264ee5e93e3885b89dfb4
LAHIRU <Vikasithasouth@gmail.com>
2022-10-13 02:30:32 +0530

    changes

--- src/main/java/Utilities/Browser_Base.java
+++ src/main/java/Utilities/Browser_Base.java
@@ -10,44 +10,76 @@ import org.openqa.selenium.devtools.v104.emulation.Emulation;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.opera.OperaDriver;
-import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.*;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.net.URL;
 import java.util.Optional;
 import java.util.Properties;
 
 public class Browser_Base {
-
     public static WebDriver driver;
 
 
-
-    @Parameters({"browser", "platform","url"})
+    @Parameters({"browser", "url"})
     @BeforeTest
-    public static void Intialize(String browser, String platform, String url) throws Exception {
+    public static void Intialize(String browser, String url) throws Exception {
+        
 
-        //java -jar selenium-server-4.5.0.jar hub (Start the Hub)
-        //java -jar selenium-server-4.5.0.jar node --detect-drivers true (Start the nodes with selected browser)
 
-        DesiredCapabilities desiredcapabilities = new DesiredCapabilities();
-        desiredcapabilities.setCapability(CapabilityType.BROWSER_NAME, browser);
-        desiredcapabilities.setCapability(CapabilityType.PLATFORM_NAME, platform);
-        driver = new RemoteWebDriver(new URL("http://192.168.1.100:4444"), desiredcapabilities);
 
-        driver.get(url);
-        driver.manage().window().maximize();
+        //Load Property File
+        FileInputStream objfile = new FileInputStream(System.getProperty("user.dir")+"/App.properties");
+        Properties obj = new Properties();
+        obj.load(objfile);
+
+        switch (browser) {
+            case "chrome" :
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                //System.setProperty("webdriver.chrome.driver", (System.getProperty("user.dir")+"/Drivers/chromedriver_linux64/chromedriver"));
+                //create chrome instance
+                //driver = new ChromeDriver();
+                break;
+            case "firefox" :
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+            case "edge" :
+                WebDriverManager.edgedriver().setup();
+                driver = new EdgeDriver();
+                break;
+            case "chromeheadless" :
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions option = new ChromeOptions();
+                option.setHeadless(true);
+                driver = new ChromeDriver(option);
+
+            case "CDTchrome" :
+                /*WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();*/
+                //System.setProperty("webdriver.chrome.driver", (System.getProperty("user.dir")+"/Drivers/chromedriver_linux64/chromedriver"));
+                //driver = new ChromeDriver();
+                //DevTools devTools = driver.getDevTools();
+                //devTools.createSession();
+                //send command to CDP Methods-> CDP Methods will invoke and get access to chrome dev tools
+                //devTools.send(Emulation.setDeviceMetricsOverride(600, 1000, 50, true, java.util.Optional.empty(), java.util.Optional.empty(), java.util.Optional.empty(), java.util.Optional.empty(), java.util.Optional.empty(), java.util.Optional.empty(), java.util.Optional.empty(), java.util.Optional.empty(), Optional.empty()));
+
+                break;
+
+            default:
+                throw new Exception("Browser is not correct");
+
 
 
         }
+        driver.get(url);
+        //driver.navigate().to(url);
+        driver.manage().window().maximize();
 
 
-
-
+    }
 
     @AfterTest
     public static void close() {
